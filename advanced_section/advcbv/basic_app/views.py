d38baf18e2291ff17f5d7015a405a389c35b2901
Ehsan Fani <efani@benhur.de>
2020-11-19 15:06:09 +0100

    use content injection in view

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View,TemplateView
 from django.http import HttpResponse
 
-
+#1st approach
 # Create your views here.
 # def index(request):
 #   return render(request,'index.html')
 
-class CBView(View):
-    def get(self, request):
-        return HttpResponse("Class based Views")
+#2nd approach
+#class CBView(View):
+ #   def get(self, request):
+  #      return HttpResponse("Class based Views")
+
+#3rd and best approach
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['injectme']='BASIC INJECTION!'
+        return context
