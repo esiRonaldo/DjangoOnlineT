22517c2978beb503f9b6f1c63274dd910965fc61
Ehsan Fani <efani@benhur.de>
2020-11-20 13:06:53 +0100

    Debug toolbar added and worked on advanceProject

--- advanced_section/advcbv/advcbv/urls.py
+++ advanced_section/advcbv/advcbv/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
@@ -24,3 +25,9 @@ urlpatterns = [
     path('basic_app', include('basic_app.urls', namespace='basic_app')),
     url(r'^$', views.IndexView.as_view()),
 ]
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+                      path('__debug__/', include((debug_toolbar.urls)))
+                  ] + urlpatterns
